56b8d147dd1fc1ba3314f1071f61b17c9fb83d21
Rodionova Ksenia <st132496@student.spbu.ru>
2025-05-14 21:11:36 +0300

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -6,5 +6,5 @@ using Vector = Eigen::VectorXd;
 
 Matrix  read (const std::string& path);
 void    write(const std::string& path, const Vector& x);
-Vector  Guess(const Matrix& A);
+Vector  Guess(const Matrix A);
 Matrix  randomSystem(int n, unsigned seed = 42);
