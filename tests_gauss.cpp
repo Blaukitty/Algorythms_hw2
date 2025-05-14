23747b81f3652e2b6c11504dad7683eb2cc8d0aa
Rodionova Ksenia <st132496@student.spbu.ru>
2025-05-14 22:14:57 +0300

    Update tests_gauss.cpp

--- tests_gauss.cpp
+++ tests_gauss.cpp
@@ -1,14 +1,15 @@
-//#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch_all.hpp>
 #include "Matrix.hpp"          
-#include <Eigen/Dense>
+//#include <Eigen/Dense>
 
 TEST_CASE("3×3 hand example") {
     Matrix A(3,4);
     A <<  2, 1,-1,  8,
          -3,-1, 2,-11,
          -2, 1, 2, -3;
-    Vector expected(3); expected << 2,-3,1;
+    Vector expected(3); 
+    expected << 2,-3,1;
 
     REQUIRE( Guess(A).isApprox(expected) );
 }
@@ -16,8 +17,8 @@ TEST_CASE("3×3 hand example") {
 TEST_CASE("Compare with Eigen solver") {
     int n = 100;
     auto sys = randomSystem(n, 123);
-    auto x = Guess(sys);
-    auto A = sys.block(0,0,n,n);
-    auto b = sys.col(n);
+    Vector x = Guess(sys);
+    Matrix M = sys.block(0,0,n,n);
+    Vector b = sys.col(n);
     REQUIRE(x.isApprox(A.fullPivLu().solve(b), 1e-8));
 }
