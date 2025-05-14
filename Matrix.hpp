4e3db2d5262f49cc6da1e90b9de13e7fa32740e7
Rodionova Ksenia <st132496@student.spbu.ru>
2025-05-14 20:11:24 +0300

    Create Matrix.hpp

---
+++ Matrix.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <Eigen/Dense>
+#include <string>
+using Matrix = Eigen::MatrixXd;
+using Vector = Eigen::VectorXd;
+
+Matrix  read (const std::string& path);
+void    write(const std::string& path, const Vector& x);
+Vector  Guess(const Matrix& A);
+Matrix  randomSystem(int n, unsigned seed = 42);
